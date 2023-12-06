91eca38728288b59b40c7631d4052e4e9b34e92a
Thiago Johnatas Paiva Da Silva <tjpdsilva@timbrasil.com.br>
2023-12-06 18:11:37 -0300

    Novas aulas

--- Curso_Python_3/aula32.py
+++ Curso_Python_3/aula32.py
@@ -3,3 +3,15 @@ Repetição
 For - While
 """
 
+v_count = 0         
+
+while v_count <=10:
+    v_count += 1 
+    if v_count  == 6:
+       print(f'Não vou mostarar {v_count}') 
+       # break    # Sai do While e não faz mais o loop.
+       continue # Volta para o While para testar a proxima condição.
+    print(f'Estou no {v_count}')
+    
+    
+    
---
+++ Curso_Python_3/aula35.py
@@ -0,0 +1,23 @@
+frase = 'Teste de contagem de letras para verificar qual' \
+'letra apareceu mais vezes. E vamos verificar'
+frase = frase.replace(" ","").lower()
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual: str = frase[i]
+
+    qtd_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes <= qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
