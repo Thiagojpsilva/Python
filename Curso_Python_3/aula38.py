0ce394a85c77db2770054edbb96549303b57baaa
Thiago Johnatas Paiva Da Silva <tjpdsilva@timbrasil.com.br>
2023-12-11 14:45:22 -0300

    Correções + inclusão do codigo Preguntas e respostas

---
+++ Curso_Python_3/aula38.py
@@ -0,0 +1,21 @@
+# Exercícios com funções
+# def duplica(i):
+#     return(i*2)
+# def triplica(i):
+#     return(i*3)
+# def quadruplica(i):
+#     return(i*4)
+
+def criar_multiplicador(mult):
+    def multiplicar(numero):
+        return numero * mult
+    return multiplicar
+
+#Main 
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadriplica = criar_multiplicador(4)
+
+print(duplicar(2),triplicar(2),quadriplica(2))
+
+
--- Curso_Python_3/teste_Code.py
+++ Curso_Python_3/teste_Code.py
@@ -1,3 +1,19 @@
-cpf = '01717406106'
-lista_cpf = list(map(int,cpf))
-print(lista_cpf[:10])
+perguntas = [
+        {
+            'Pergunta': 'Quanto é 2+2?',
+            'Opções': ['1', '3', '4', '5'],
+            'Resposta': '4',
+        },
+        {
+            'Pergunta': 'Quanto é 5*5?',
+            'Opções': ['25', '55', '10', '51'],
+            'Resposta': '25',
+        },
+        {
+            'Pergunta': 'Quanto é 10/2?',
+            'Opções': ['4', '5', '2', '1'],
+            'Resposta': '5',
+        },
+    ]
+pergunta = (perguntas[0])['Pergunta']
+print(pergunta)
