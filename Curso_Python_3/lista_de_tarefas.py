c4b6acd123e7610201311025cb2af41c839ab1e5
Thiago Johnatas Paiva Da Silva <tjpdsilva@timbrasil.com.br>
2023-12-27 00:00:16 -0300

    Listar de tarefas

--- Curso_Python_3/lista_de_tarefas.py
+++ Curso_Python_3/lista_de_tarefas.py
@@ -9,157 +9,133 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 import os
+import json
 
-primery_list = []
-next_list = []
+PATH = 'C:\\Users\\f8084478\OneDrive - TIM\\Pessoal\\GIT\\Pessoal\\Python\\Curso_Python_3\\arquivos\\'
+ARQUIVOS = 'tarefas.json'
+local = PATH+ARQUIVOS
 
+def read_task(path):
+    data = []
+    global primery_list
+    try:
+        with open(path,'r',encoding='utf-8') as arquivo:
+            data= json.load(arquivo)   
+    except FileNotFoundError:
+        print ('Arquivo não existe!')
+        primery_list = []
+        update_file()
+    return list(data)
+
+def update_file():
+    global PATH
+    global ARQUIVOS
+    global primery_list
+    local =  PATH + ARQUIVOS
+    with open(local, 'w', encoding='utf8') as arquivo:
+        data = json.dump(primery_list, arquivo, indent=2, ensure_ascii=False)
+    return data
+
+    
 def clean_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
     print('Comandos: listar, desfazer, refazer e editar \nDigite um comando:')
     
     
 def list_task():
-    print('\n','-'*30,'\n') 
-    print('\n'.join(map(str, primery_list)))
-    print('\n','-'*30)
-    input('Click Enter Para Continuar.')
+    
+    if not primery_list:
+        print('A lista está Vazia!') 
+        input('Click Enter Para Continuar.')
+    else:
+        print('\n','-'*30,'\n') 
+        print('\n'.join(map(str, primery_list)))
+        print('\n','-'*30)
+        input('Click Enter para continuar.')
 
-def exec_func(funcao, *args):
-    result = funcao(*args)
-    return result
+def exec_func(funcao):
+    result = funcao()
+    return None
 
 def undo_list():
     if primery_list:
         removed_task = primery_list.pop()
         print(f'Tarefa removida: {removed_task}')
+        update_file()
     else:
-        print('\nA lista está vazia. Não há tarefas para desfazer.\n')
-    
+        print('\nNão há tarefas para desfazer.\n')
+         
+    exec_func(list_task)
+       
 def remake_list():
     global next_list
     if len(primery_list) < len(next_list):
         if len(primery_list) == 0:
            primery_list.append(next_list[0])
+           update_file()
         else:
-           primery_list.append(next_list[len(primery_list)])  
+           primery_list.append(next_list[len(primery_list)])
+           update_file()  
     else:
         print(f'\nNão há tarefas para serem refeitas.\n')      
-
-def add_task(*args):
+        
+    exec_func(list_task)
+    
+def add_task():
     global next_list
-    primery_list.append(*args)
+    item = input('Qual tarefa será adicionada: ')
+    primery_list.append(item.strip())
     next_list = primery_list.copy()
-    # print(f'Print task: {*args}')
+    update_file()
+    exec_func(list_task)
 
 clean_screen()
 
+primery_list = read_task(local)
+next_list = read_task(local)
+
 while True:
-    option = input()
+    option: str = input().upper()
     
-    if option.upper() == 'LISTAR':
-        if not primery_list:
-            print('A lista está Vazia!')
-            input('Click Enter Para Continuar.')
-        else:
-            exec_func(list_task)
-        
-    elif option.upper() == 'EDITAR':
-        item = input('Qual tarefa será adicionada: ')
-        exec_func(add_task, item)
-        exec_func(list_task)
+    comandos={
+            'LISTAR': lambda: exec_func(list_task),
+            'EDITAR': lambda: exec_func(add_task),
+            'DESFAZER': lambda: exec_func(undo_list),
+            'REFAZER': lambda: exec_func(remake_list)
+    }
     
-    elif option.upper() == 'DESFAZER':
-        exec_func(undo_list)
-        exec_func(list_task)
-
-    elif option.upper() == 'REFAZER':
-        exec_func(remake_list)
-        exec_func(list_task)
-
+    if option in ('LISTAR','EDITAR','DESFAZER','REFAZER'):
+        comando = comandos.get(option)()   
+            
     else:
         print('Comando desconhecido comandos válidos (listar, desfazer, refazer e editar)!')
-        input('Click Enter Para Continuar.')
-    clean_screen()    
-#######
-## Codigo Curso
-######
+        input('Click Enter para continuar.')
 
+    clean_screen()
 
-# # desfazer = [] -> Refazer ['caminhar', 'fazer café']
-# # refazer = todo ['fazer café']
-# # refazer = todo ['fazer café', 'caminhar']
-# import os
+    continue
 
+    # if option.upper() == 'LISTAR':
+    #     if not primery_list:
+    #         print('A lista está Vazia!')
+    #         input('Click Enter Para Continuar.')
+    #     else:
+    #         exec_func(list_task)
+        
+    # elif option.upper() == 'EDITAR':
+    #     item = input('Qual tarefa será adicionada: ')
+    #     exec_func(add_task, item)
+    #     exec_func(list_task)
+    
+    # elif option.upper() == 'DESFAZER':
+    #     exec_func(undo_list)
+    #     exec_func(list_task)
 
-# def listar(tarefas):
-#     print()
-#     if not tarefas:
-#         print('Nenhuma tarefa para listar')
-#         return
+    # elif option.upper() == 'REFAZER':
+    #     exec_func(remake_list)
+    #     exec_func(list_task)
 
-#     print('Tarefas:')
-#     for tarefa in tarefas:
-#         print(f'\t{tarefa}')
-#     print()
-
-
-# def desfazer(tarefas, tarefas_refazer):
-#     print()
-#     if not tarefas:
-#         print('Nenhuma tarefa para desfazer')
-#         return
-
-#     tarefa = tarefas.pop()
-#     print(f'{tarefa=} removida da lista de tarefas.')
-#     tarefas_refazer.append(tarefa)
-#     print()
-
-
-# def refazer(tarefas, tarefas_refazer):
-#     print()
-#     if not tarefas_refazer:
-#         print('Nenhuma tarefa para refazer')
-#         return
-
-#     tarefa = tarefas_refazer.pop()
-#     print(f'{tarefa=} adicionada na lista de tarefas.')
-#     tarefas.append(tarefa)
-#     print()
-
-
-# def adicionar(tarefa, tarefas):
-#     print()
-#     tarefa = tarefa.strip()
-#     if not tarefa:
-#         print('Você não digitou uma tarefa.')
-#         return
-#     print(f'{tarefa=} adicionada na lista de tarefas.')
-#     tarefas.append(tarefa)
-#     print()
-
-
-# tarefas = []
-# tarefas_refazer = []
-
-# while True:
-#     print('Comandos: listar, desfazer e refazer')
-#     tarefa = input('Digite uma tarefa ou comando: ')
-
-#     if tarefa == 'listar':
-#         listar(tarefas)
-#         continue
-#     elif tarefa == 'desfazer':
-#         desfazer(tarefas, tarefas_refazer)
-#         listar(tarefas)
-#         continue
-#     elif tarefa == 'refazer':
-#         refazer(tarefas, tarefas_refazer)
-#         listar(tarefas)
-#         continue
-#     elif tarefa == 'clear':
-#         os.system('clear')
-#         continue
-#     else:
-#         adicionar(tarefa, tarefas)
-#         listar(tarefas)
-#         continue
+    # else:
+    #     print('Comando desconhecido comandos válidos (listar, desfazer, refazer e editar)!')
+    #     input('Click Enter Para Continuar.')
+    # clean_screen()    
