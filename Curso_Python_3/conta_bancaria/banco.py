7f2a250e88b40b7f77708a115d81cefbf4f0d7a1
Thiago Johnatas Paiva Da Silva <tjpdsilva@timbrasil.com.br>
2024-01-02 11:47:14 -0300

    Ajuste de scripts

--- Curso_Python_3/conta_bancaria/banco.py
+++ Curso_Python_3/conta_bancaria/banco.py
@@ -5,6 +5,12 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 
 class Conta(abc.ABC):
+    """_summary_
+
+    Args:
+        abc (_type_): _description_
+    """
+
     def __init__(self, agencia: int, num_conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.num_conta = num_conta
@@ -12,22 +18,50 @@ class Conta(abc.ABC):
 
     @abc.abstractmethod
     def sacar(self, valor: float) -> float:
+        """_summary_
+
+        Args:
+            valor (float): _description_
+
+        Returns:
+            float: _description_
+        """
         ...
 
     def deposito(self, valor: float) -> None:
+        """_summary_
+
+        Args:
+            valor (float): _description_
+        """
         self.saldo += valor
         self.detalhes(f'(DEPÓSITO DE {locale.currency(valor, grouping=True)})')
 
     def detalhes(self, msg: str = '') -> None:
+        """_summary_
+
+        Args:
+            msg (str, optional): _description_. Defaults to ''.
+
+        Returns:
+            _type_: _description_
+        """
         if self.__class__.__name__ == 'ContaPoupanca':
             print(f'O seu saldo é: {locale.currency(
                 self.saldo, grouping=True)} {msg}')
             return None
         print(f'O seu saldo é: {locale.currency(self.saldo, grouping=True)} {
-              msg} \nLimite disponivel de: {locale.currency(self.limite, grouping=True)}')  # type: ignore
+              msg} \nLimite disponivel de: {locale.currency(self.limite,
+                                                            grouping=True)}')
 
 
 class ContaPoupanca (Conta):
+    """_summary_
+
+    Args:
+        Conta (_type_): _description_
+    """
+
     def sacar(self, valor):
 
         valor_pos_saque = self.saldo - valor
@@ -44,18 +78,48 @@ class ContaPoupanca (Conta):
 
 
 class ContaCorrente(Conta):
+    """_summary_
+
+    Args:
+        Conta (_type_): _description_
+    """
+
     def __init__(self, agencia: int, num_conta: int, saldo: float = 0, limite: float = 0):
+        """_summary_
+
+        Args:
+            agencia (int): _description_
+            num_conta (int): _description_
+            saldo (float, optional): _description_. Defaults to 0.
+            limite (float, optional): _description_. Defaults to 0.
+        """
         super().__init__(agencia, num_conta, saldo)
         self.limite = limite
         self.limite_inicial = limite
 
     def deposito(self, valor):
+        """_summary_
+
+        Args:
+            valor (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         if (self.limite != self.limite_inicial) and ((self.saldo + valor) >= 0):
             self.limite = self.limite_inicial
             return super().deposito(valor)
         super().deposito(valor)
 
     def sacar(self, valor):
+        """_summary_
+
+        Args:
+            valor (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         valor_pos_saque = self.saldo - valor
         valor_pos_s_limite = self.limite - valor
 
--- Curso_Python_3/conta_bancaria/pessoa.py
+++ Curso_Python_3/conta_bancaria/pessoa.py
@@ -1,14 +1,46 @@
-import abc
+from tomlkit import integer
 
 
 class Pessoa:
+    """_summary_
+    """
+
+    def __init__(self, nome: str, idade: int) -> None:
+        """
+        Args:
+            nome (str): _description_
+            idade (int): _description_
+        """
+        self.nome = nome
+        self.idade = idade
 
     def pessoa_setter(self):
-        ...
+        """_summary_
+        """
+    @property
+    def nome(self):
+        """_summary_
+        """
+        return self._nome
 
-    def pessoa_getter(self):
-        ...
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
+
+    @property
+    def idade(self):
+        """_summary_
+        """
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade: int):
+        self._idade = idade
 
 
 class Cliente(Pessoa):
-    ...
+    """_summary_
+
+    Args:
+        Pessoa (_type_): _description_
+    """
